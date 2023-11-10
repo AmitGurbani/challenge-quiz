7fb7d4218cc403c5464f3861b2577e36f2168e6e
Amit Gurbani <amit1994.gurbani@gmail.com>
2023-11-10 12:59:51 +0530

    Fix: Deprecated render

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,10 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { unmountComponentAtNode } from 'react-dom'
 import App from './App'
+import { render } from '@testing-library/react'
 
-it('renders without crashing', () => { // eslint-disable-line
+it('renders without crashing', () => {
   const div = document.createElement('div')
-  ReactDOM.render(<App />, div)
-  ReactDOM.unmountComponentAtNode(div)
+  render(<App />, div)
+  unmountComponentAtNode(div)
 })
